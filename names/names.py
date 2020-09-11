fc1d27dcfb44a05be38bc610ad7feb5495436309
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-11 12:31:26 -0400

    Stretch

--- names/names.py
+++ names/names.py
@@ -13,16 +13,25 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# What's the best time you can accomplish?  There are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# duplicates = [i for i, j in zip(names_1, names_2) if i == j]
+
+# ------------------------------------
+# runtime: 0.002496957778930664 seconds
+# runtime: 0.0024118423461914062 seconds
+# runtime: 0.002477884292602539 seconds
+
+# approximate runtime : 0.0025 seconds
