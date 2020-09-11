e9b4f9277520507d6093e638fb98eaea64b7e3c2
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-11 14:13:16 -0400

    Update ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -10,26 +10,26 @@ class RingBuffer:
         self.counter = 0
         self.pointer = 0
         self.buffer = []
-        self.root = None
+        # self.root = None
 
     def append(self, item):
         if self.counter < self.capacity:
             self.buffer.append(item)
             self.counter += 1
             self.pointer += 1
-            self.root = ListNode(item)
-            self.root.next = self.root
+            # self.root = ListNode(item)
+            # self.root.next = self.root
         elif self.counter >= self.capacity:
             if self.pointer == self.capacity:
                 self.pointer = 0
             self.buffer[self.pointer] = item
-            current = self.root
-            dif = self.counter - self.capacity
-            if dif > 0:
-                while dif > 0:
-                    current = current.next
-                    dif -= 1
-            current.value = item
+            # current = self.root
+            # dif = self.counter - self.capacity
+            # if dif > 0:
+            #     while dif > 0:
+            #         current = current.next
+            #         dif -= 1
+            # current.value = item
             self.counter += 1
             self.pointer += 1
 
