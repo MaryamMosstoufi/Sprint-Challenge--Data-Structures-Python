23ee9f25703dac8bde5a2a8c6840084ed9ac9e35
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-11 13:48:23 -0400

    Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.counter = 0
+        self.pointer = 0
+        self.buffer = []
 
     def append(self, item):
-        pass
+        if self.counter < self.capacity:
+            self.buffer.append(item)
+            self.counter += 1
+            self.pointer += 1
+        elif self.counter >= self.capacity:
+            if self.pointer == self.capacity:
+                self.pointer = 0
+            self.buffer[self.pointer] = item
+            self.counter += 1
+            self.pointer += 1
 
     def get(self):
-        pass
+        return(self.buffer)
